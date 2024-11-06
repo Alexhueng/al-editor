72b2c3ba561bcbfbb656ec8ce955928dceb901ab
Alexhueng <749845548@qq.com>
2024-11-07 00:57:39 +0800

    feat: init and init graph

---
+++ src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center">
+    <div
+      class="text-3xl text-center mt-[20px] bg-white border border-[#dedede] w-fit px-2 rounded-lg cursor-pointer"
+      @click="router.push('/editor')"
+    >
+      to editor
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<style lang="scss" scoped></style>
