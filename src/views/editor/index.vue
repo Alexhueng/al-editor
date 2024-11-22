25fcbaa60d7f6a179b3057a26839251d101a0264
Alexhueng <749845548@qq.com>
2024-11-23 01:22:44 +0800

    feat: 新增角度控制; 新增画布控制面板;

--- src/views/editor/index.vue
+++ src/views/editor/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="flex w-full h-full">
-    <div id="stencil"></div>
-    <div id="graph-container"></div>
+    <div id="stencil" :style="{ width: stencilWidth + 'px' }"></div>
+    <div
+      id="graph-container"
+      :style="{ width: `calc(100% - ${stencilWidth + controlPanelWidth}px)` }"
+    />
+    <div class="graph-control-panel" :style="{ width: controlPanelWidth + 'px' }">
+      <GraphControlPanel />
+    </div>
+
     <TeleportContainer />
 
     <OptionsPanel :mask="false" />
@@ -23,12 +30,18 @@ import { register, getTeleport } from '@antv/x6-vue-shape'
 import { useRegister } from '@/views/nodes/custom-1/index'
 import OptionsPanel from '@/views/optionsPanel/index.vue'
 import { usePanelStore } from '@/stores/panel'
+import { useGraphStore } from '@/stores/graph'
+import GraphControlPanel from '@/views/optionsPanel/graph.vue'
 
 const TeleportContainer = getTeleport()
 const panelStore = usePanelStore()
+const graphStore = useGraphStore()
+
+const stencilWidth = 300
+const controlPanelWidth = 500
 
 onMounted(() => {
-  preWork()
+  // preWork()
 
   const graph = new Graph({
     container: document.getElementById('graph-container')!,
@@ -91,6 +104,9 @@ onMounted(() => {
     },
   })
   // #endregion
+
+  graphStore.setGraph(graph)
+  console.log(graph)
 
   // #region 使用插件
   graph
@@ -538,70 +554,120 @@ onMounted(() => {
   stencil.load(imageNodes, 'group2')
   // #endregion
 
-  function preWork() {
-    // 这里协助演示的代码，在实际项目中根据实际情况进行调整
-    const container = document.getElementById('graph-container')!
-    const stencilContainer = document.createElement('div')
-    stencilContainer.id = 'stencil'
-    const graphContainer = document.createElement('div')
-    graphContainer.id = 'graph-container'
-    container?.appendChild(stencilContainer)
-    container?.appendChild(graphContainer)
+  // function preWork() {
+  //   // 这里协助演示的代码，在实际项目中根据实际情况进行调整
+  //   const container = document.getElementById('graph-container')!
+  //   const stencilContainer = document.createElement('div')
+  //   stencilContainer.id = 'stencil'
+  //   const graphContainer = document.createElement('div')
+  //   graphContainer.id = 'graph-container'
+  //   container?.appendChild(stencilContainer)
+  //   container?.appendChild(graphContainer)
 
-    insertCss(`
-    #container {
-      display: flex;
-      border: 1px solid #dfe3e8;
-    }
-    #stencil {
-      width: 300px;
-      height: 100%;
-      position: relative;
-      border-right: 1px solid #dfe3e8;
-    }
-    #graph-container {
-      width: calc(100% - 300px);
-      height: 100%;
-    }
-    .x6-widget-stencil  {
-      background-color: #fff;
-    }
-    .x6-widget-stencil-content {
-      padding: 10px;
-      padding-top: 42px;
-    }
-    .x6-widget-stencil-title {
-      background-color: #fff;
-    }
-    .x6-widget-stencil-group-title {
-      background-color: #fff !important;
-    }
-    .x6-widget-transform {
-      margin: -1px 0 0 -1px;
-      padding: 0px;
-      border: 1px solid #239edd;
-    }
-    .x6-widget-transform > div {
-      border: 1px solid #239edd;
-    }
-    .x6-widget-transform > div:hover {
-      background-color: #3dafe4;
-    }
-    .x6-widget-transform-active-handle {
-      background-color: #3dafe4;
-    }
-    .x6-widget-transform-resize {
-      border-radius: 0;
-    }
-    .x6-widget-selection-inner {
-      border: 1px solid #239edd;
-    }
-    .x6-widget-selection-box {
-      opacity: 0;
-    }
-  `)
-  }
+  //   insertCss(`
+  //   #container {
+  //     display: flex;
+  //     border: 1px solid #dfe3e8;
+  //   }
+  //   #stencil {
+  //     width: 300px;
+  //     height: 100%;
+  //     position: relative;
+  //     border-right: 1px solid #dfe3e8;
+  //   }
+  //   // #graph-container {
+  //   //   width: calc(100% - 300px);
+  //   //   height: 100%;
+  //   // }
+  //   .x6-widget-stencil  {
+  //     background-color: #fff;
+  //   }
+  //   .x6-widget-stencil-content {
+  //     padding: 10px;
+  //     padding-top: 42px;
+  //   }
+  //   .x6-widget-stencil-title {
+  //     background-color: #fff;
+  //   }
+  //   .x6-widget-stencil-group-title {
+  //     background-color: #fff !important;
+  //   }
+  //   .x6-widget-transform {
+  //     margin: -1px 0 0 -1px;
+  //     padding: 0px;
+  //     border: 1px solid #239edd;
+  //   }
+  //   .x6-widget-transform > div {
+  //     border: 1px solid #239edd;
+  //   }
+  //   .x6-widget-transform > div:hover {
+  //     background-color: #3dafe4;
+  //   }
+  //   .x6-widget-transform-active-handle {
+  //     background-color: #3dafe4;
+  //   }
+  //   .x6-widget-transform-resize {
+  //     border-radius: 0;
+  //   }
+  //   .x6-widget-selection-inner {
+  //     border: 1px solid #239edd;
+  //   }
+  //   .x6-widget-selection-box {
+  //     opacity: 0;
+  //   }
+  // `)
+  // }
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#container {
+  display: flex;
+  border: 1px solid #dfe3e8;
+}
+#stencil {
+  height: 100%;
+  position: relative;
+  border-right: 1px solid #dfe3e8;
+}
+// #graph-container {
+//   width: calc(100% - 300px);
+//   height: 100%;
+// }
+.x6-widget-stencil {
+  background-color: #fff;
+}
+.x6-widget-stencil-content {
+  padding: 10px;
+  padding-top: 42px;
+}
+.x6-widget-stencil-title {
+  background-color: #fff;
+}
+.x6-widget-stencil-group-title {
+  background-color: #fff !important;
+}
+.x6-widget-transform {
+  margin: -1px 0 0 -1px;
+  padding: 0px;
+  border: 1px solid #239edd;
+}
+.x6-widget-transform > div {
+  border: 1px solid #239edd;
+}
+.x6-widget-transform > div:hover {
+  background-color: #3dafe4;
+}
+.x6-widget-transform-active-handle {
+  background-color: #3dafe4;
+}
+.x6-widget-transform-resize {
+  border-radius: 0;
+}
+.x6-widget-selection-inner {
+  border: 1px solid #239edd;
+}
+.x6-widget-selection-box {
+  opacity: 0;
+}
+</style>
--- src/views/optionsPanel/index.vue
+++ src/views/optionsPanel/index.vue
@@ -9,69 +9,7 @@
     placement="right"
   >
     <n-drawer-content title="节点属性">
-      <n-form :model="form" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
-        <n-form-item label="节点内容">
-          <n-input
-            v-model:value="data.text"
-            placeholder="请输入节点内容"
-            @input="handleNodeContentChange"
-          />
-        </n-form-item>
-
-        <n-form-item label="层级">
-          <n-input-number
-            v-model:value="zIndex"
-            placeholder="请输入层级"
-            class="w-full"
-            @update:value="handleZIndexChange"
-          />
-        </n-form-item>
-
-        <n-form-item label="位置">
-          <n-space>
-            <n-input-number
-              v-model:value="position.x"
-              placeholder="x"
-              @update:value="(value: number) => handleUpdatePosition(value, 'x')"
-              @input="(evt: InputEvent) => handleNodePostionChange(evt, 'x')"
-            />
-            <n-input-number
-              v-model:value="position.y"
-              placeholder="y"
-              @update:value="(value: number) => handleUpdatePosition(value, 'y')"
-              @input="(evt: InputEvent) => handleNodePostionChange(evt, 'y')"
-            />
-          </n-space>
-        </n-form-item>
-
-        <n-form-item label="宽高">
-          <n-space>
-            <n-input-number
-              v-model:value="size.width"
-              placeholder="请输入宽"
-              @update:value="(value: number) => handleUpdateSize(value, 'w')"
-              @input="(evt: InputEvent) => handleNodeSizeChange(evt, 'w')"
-            />
-            <n-input-number
-              v-model:value="size.height"
-              placeholder="请输入高"
-              @update:value="(value: number) => handleUpdateSize(value, 'h')"
-              @input="(evt: InputEvent) => handleNodeSizeChange(evt, 'h')"
-            />
-          </n-space>
-        </n-form-item>
-
-        <n-form-item label="背景颜色">
-          <div class="w-full">
-            <n-color-picker
-              v-model:value="data.backgroundColor"
-              :modes="['rgb', 'hex', 'hsl']"
-              @update:value="handleUpdateColor"
-            />
-            <n-input v-model:value="data.backgroundColor" @update:value="handleUpdateColor" />
-          </div>
-        </n-form-item>
-      </n-form>
+      <NodeOptions />
     </n-drawer-content>
   </n-drawer>
 </template>
@@ -79,81 +17,19 @@
 import { ref, watch, computed, reactive } from 'vue'
 import { usePanelStore } from '@/stores/panel'
 import type { Node, Size } from '@antv/x6'
-
-type SizeType = 'w' | 'h'
+import NodeOptions from './node.vue'
 
 const panelStore = usePanelStore()
-const form = reactive({
-  width: 0,
-  height: 0,
-})
-const size = ref({} as Size)
-const data = ref({} as any)
-const position = ref({} as any)
-const zIndex = ref(0)
 
 const node = computed(() => {
   return panelStore.getNode()!
 })
 
-watch(
-  () => panelStore.panelVisible,
-  (value) => {
-    if (value) {
-      size.value = node.value!.getSize()
-      data.value = node.value!.getData() || {}
-      position.value = node.value?.getPosition()
-      zIndex.value = node.value?.getZIndex()!
-    }
-  },
-)
-
-const handleNodeContentChange = (value: string) => {
-  // const node = panelStore.getNode()
-  // node?.setProp({
-  //   text: value,
-  // })
-
-  node.value!.setData({
-    text: value,
-  })
-}
-
-const handleZIndexChange = (value: number) => {
-  console.log(value)
-  node.value.setZIndex(value)
-}
-
-const handleUpdateSize = (value: number, type: SizeType) => {
-  resize(value, type)
-}
-
-const resize = (value: number, type: SizeType) => {
-  const size = node.value.getSize()
-  node.value.resize(type === 'w' ? value : size.width, type === 'h' ? value : size.height)
-}
-
-const handleNodeSizeChange = (evt: InputEvent, type: SizeType) => {
-  const value = Number((evt.target as HTMLInputElement).value)
-  resize(value, type)
-}
-
-const handleUpdateColor = (value: string) => {
-  node.value.setData({
-    backgroundColor: value,
-  })
-}
-
-type positionType = 'x' | 'y'
-const handleUpdatePosition = (value: number, type: positionType) => {
-  node.value.setPosition(
-    type === 'x' ? value : position.value.x,
-    type === 'y' ? value : position.value.y,
-  )
-}
-
-const handleNodePostionChange = (evt: InputEvent, type: positionType) => {
-  const value = Number((evt.target as HTMLInputElement).value)
-  handleUpdatePosition(value, type)
-}
+// watch(
+//   () => panelStore.panelVisible,
+//   (value) => {
+//     if (value) {
+//     }
+//   },
+// )
 </script>
